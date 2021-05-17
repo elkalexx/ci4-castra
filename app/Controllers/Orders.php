9bec1d9c3c82196b28b6e920541dadaaf748e3db
elkalexx <kevin.schneider@mercari.bg>
2021-05-17 15:59:59 +0300

    Ion Auth

--- app/Controllers/Orders.php
+++ app/Controllers/Orders.php
@@ -3,8 +3,42 @@
 
 namespace App\Controllers;
 
+use CodeIgniter\Controller;
+use IonAuth\Libraries\IonAuth;
 
-class Orders
+class Orders extends Controller
 {
+    public function index()
+    {
+        helper('cookie');
+        set_cookie([
+            'name' => "URL_Cookie",
+            'value' => "Orders/hello",
+            'expire' => time() + 1000,
+            'httponly' => false
+        ]);
 
+        return redirect()->to('auth/login')->withCookies();
+    }
+
+    public function hello()
+    {
+        $ionAuth = new IonAuth;
+        if ($ionAuth->isAdmin()) {
+            echo "ADMIN";
+        } else {
+            echo "No Admin";
+        }
+    }
+
+    public function hellohello()
+    {
+        $ionauth = new IonAuth;
+
+    }
+
+    public function test()
+    {
+        echo view('test');
+    }
 }
---
+++ app/Views/test.php
@@ -0,0 +1,9 @@
+<head>
+    <?= service('assets')->css() ?>
+</head>
+<body>
+
+<a href="<?= base_url() . "/auth/login" ?>">here</a>
+
+<?= service('assets')->js() ?>
+</body>
